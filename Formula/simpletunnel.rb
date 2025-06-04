f3d92c6d22567829398dad14ebfed072a7e689a9
val polyakov <val@polyakov.me>
2025-06-04 16:28:07 -0500

    Update to v2.2.0 - WebSocket support for free tier

--- Formula/simpletunnel.rb
+++ Formula/simpletunnel.rb
@@ -1,27 +1,27 @@
 class Simpletunnel < Formula
   desc "Expose local services to the internet with automatic HTTPS"
   homepage "https://simpletunnel.com"
-  version "2.1.0"
+  version "2.2.0"
   license "Proprietary"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/simple-tunnel/releases/releases/download/v2.1.0/tunnel-client-darwin-arm64"
-    sha256 "70f95533925d401d8b0f5e2cb640a79762108caa57df18bcbc2bf5eabafea942"
+    url "https://github.com/simple-tunnel/releases/releases/download/v2.2.0/simpletunnel-darwin-arm64"
+    sha256 "5ef1d44da8b06fe08e7e500bc4dde3490b33c5333470f174e40444d931767746"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/simple-tunnel/releases/releases/download/v2.1.0/tunnel-client-darwin-amd64"
-    sha256 "9a320f453b4eae4d37f8170cd683d5771e071e6940942b945d49a736160a7cc9"
+    url "https://github.com/simple-tunnel/releases/releases/download/v2.2.0/simpletunnel-darwin-amd64"
+    sha256 "989c19d7f7c11a2707429a8e14399b03d2aba866d8b40cb65ebe16b972f7fbbf"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/simple-tunnel/releases/releases/download/v2.1.0/tunnel-client-linux-amd64"
-    sha256 "314736f7fc5496c79cd912f597081ee4efa0893dc0eb440f8e7d49b6639696dc"
+    url "https://github.com/simple-tunnel/releases/releases/download/v2.2.0/simpletunnel-linux-amd64"
+    sha256 "024e063822e2ae8d2e4d4af591fde459e1ecae57f61d63267d2bed5517f839fb"
   end
 
   def install
     if OS.mac? && Hardware::CPU.arm?
-      bin.install "tunnel-client-darwin-arm64" => "simpletunnel"
+      bin.install "simpletunnel-darwin-arm64" => "simpletunnel"
     elsif OS.mac? && Hardware::CPU.intel?
-      bin.install "tunnel-client-darwin-amd64" => "simpletunnel"
+      bin.install "simpletunnel-darwin-amd64" => "simpletunnel"
     elsif OS.linux?
-      bin.install "tunnel-client-linux-amd64" => "simpletunnel"
+      bin.install "simpletunnel-linux-amd64" => "simpletunnel"
     end
   end
 

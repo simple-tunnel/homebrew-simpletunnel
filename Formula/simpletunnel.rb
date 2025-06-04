7b1d61732f50e7baf221cded08fed55ae606a431
val polyakov <val@polyakov.me>
2025-06-03 23:30:15 -0500

    Update SimpleTunnel to v1.3.0
    - Added Intel Mac (darwin-amd64) binary support
    - WebSocket support for Streamlit, Jupyter, Socket.io
    - Password protection with -password flag
    - Kubernetes controller improvements

--- Formula/simpletunnel.rb
+++ Formula/simpletunnel.rb
@@ -1,17 +1,17 @@
 class Simpletunnel < Formula
   desc "Expose local services to the internet with automatic HTTPS"
   homepage "https://simpletunnel.com"
-  version "1.2.0"
+  version "1.3.0"
   license "Proprietary"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/simple-tunnel/releases/releases/download/v1.2.0/tunnel-client-darwin-arm64"
+    url "https://github.com/simple-tunnel/releases/releases/download/v1.3.0/tunnel-client-darwin-arm64"
     sha256 "5ef1d44da8b06fe08e7e500bc4dde3490b33c5333470f174e40444d931767746"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/simple-tunnel/releases/releases/download/v1.2.0/tunnel-client-darwin-amd64"
-    sha256 "PLACEHOLDER_INTEL_MAC_SHA256"
+    url "https://github.com/simple-tunnel/releases/releases/download/v1.3.0/tunnel-client-darwin-amd64"
+    sha256 "989c19d7f7c11a2707429a8e14399b03d2aba866d8b40cb65ebe16b972f7fbbf"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/simple-tunnel/releases/releases/download/v1.2.0/tunnel-client-linux-amd64"
+    url "https://github.com/simple-tunnel/releases/releases/download/v1.3.0/tunnel-client-linux-amd64"
     sha256 "024e063822e2ae8d2e4d4af591fde459e1ecae57f61d63267d2bed5517f839fb"
   end
 

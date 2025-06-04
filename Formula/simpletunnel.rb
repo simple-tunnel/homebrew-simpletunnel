d119436217c5f70952e8d6675bd509c6fb5549fa
val polyakov <val@polyakov.me>
2025-06-04 12:39:34 -0500

    Update to v2.0.2 - Free tier domain fix

--- Formula/simpletunnel.rb
+++ Formula/simpletunnel.rb
@@ -1,18 +1,18 @@
 class Simpletunnel < Formula
   desc "Expose local services to the internet with automatic HTTPS"
   homepage "https://simpletunnel.com"
-  version "2.0.1"
+  version "2.0.2"
   license "Proprietary"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/simple-tunnel/releases/releases/download/v2.0.1/simpletunnel-darwin-arm64"
-    sha256 "9d0ec7039130940242539be34a622f23db4e2c9a2ece4a811023f98e865d8560"
+    url "https://github.com/simple-tunnel/releases/releases/download/v2.0.2/simpletunnel-darwin-arm64"
+    sha256 "f78f8444d6cb2bfe064401dd1e71b61ddf1aa4ba2e2ef2dc13395ce1f1f200e8"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/simple-tunnel/releases/releases/download/v2.0.1/simpletunnel-darwin-amd64"
-    sha256 "dfbef4afe04d373be6f326d1a60d5ea5a8ccba7782e7449a47690bfbdeb68d44"
+    url "https://github.com/simple-tunnel/releases/releases/download/v2.0.2/simpletunnel-darwin-amd64"
+    sha256 "ec2880f770449c98fcca1523826c0889b999cae8fcb2f4a72d1c28dd3d851121"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/simple-tunnel/releases/releases/download/v2.0.1/simpletunnel-linux-amd64"
-    sha256 "f6855e6df256db21d34f62a8826aecd74ea4a164794bdbe62e7d0a9518b39ced"
+    url "https://github.com/simple-tunnel/releases/releases/download/v2.0.2/simpletunnel-linux-amd64"
+    sha256 "0b28080abcf6955a3d29c6ed2f6862b6d9e7e7ee150962ec33ba0e1f9221fc3d"
   end
 
   def install

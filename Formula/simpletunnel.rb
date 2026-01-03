f8ccc5b12d62ae07a5acf25e486ed366d1b0d799
LocalShare <noreply@localshare.io>
2026-01-02 20:50:37 -0600

    Update SimpleTunnel to v3.28.4

--- Formula/simpletunnel.rb
+++ Formula/simpletunnel.rb
@@ -1,16 +1,16 @@
 class Simpletunnel < Formula
   desc "Fast and secure tunneling solution for exposing local services"
   homepage "https://simpletunnel.com"
-  version "3.28.2"
+  version "3.28.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/simple-tunnel/releases/releases/download/v3.28.2/simpletunnel-darwin-arm64"
-      sha256 "95833eb88ed146bc735b23d3d4a7814f8958773c68083db692fc76f1b73e16e6"
+      url "https://github.com/simple-tunnel/releases/releases/download/v3.28.4/simpletunnel-darwin-arm64"
+      sha256 "19313dbaeaf5bd8353099f2e5ec0df9d7027e2b0a1f16b04c3aab5299fc1082c"
     else
-      url "https://github.com/simple-tunnel/releases/releases/download/v3.28.2/simpletunnel-darwin-amd64"
-      sha256 "6767629e525fe878d5763449b6ac2019b308df009ff2b63530e2bc04d544dab5"
+      url "https://github.com/simple-tunnel/releases/releases/download/v3.28.4/simpletunnel-darwin-amd64"
+      sha256 "1af022bfdac5fa227a0db51f579cc0a4930883eb32cc250b58b95bf598383ad5"
     end
   end
 
@@ -21,11 +21,11 @@ class Simpletunnel < Formula
   def caveats
     <<~EOS
       SimpleTunnel requires an API key to function.
-      
+
       1. Get your API key from https://simpletunnel.com
       2. Use SimpleTunnel:
          simpletunnel -port 3000 -key YOUR_API_KEY
-      
+
       For more information, visit https://simpletunnel.com/docs
     EOS
   end

ebd59eec0a1ca2172b946f537c73a82158d9aba1
LocalShare <noreply@localshare.io>
2026-01-08 21:16:53 -0600

    Update SimpleTunnel to v3.40.2

--- Formula/simpletunnel.rb
+++ Formula/simpletunnel.rb
@@ -1,16 +1,16 @@
 class Simpletunnel < Formula
   desc "Fast and secure tunneling solution for exposing local services"
   homepage "https://simpletunnel.com"
-  version "3.40.1"
+  version "3.40.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/simple-tunnel/releases/releases/download/v3.40.1/simpletunnel-darwin-arm64"
-      sha256 "ed589791475ee1b5ca96a35131b67291e7710f1fb18a263563b6521dad8873c1"
+      url "https://github.com/simple-tunnel/releases/releases/download/v3.40.2/simpletunnel-darwin-arm64"
+      sha256 "3e646e4b34d50a0959106184ac1671c8f9d4e7d70ea2631866c5a7f89b399623"
     else
-      url "https://github.com/simple-tunnel/releases/releases/download/v3.40.1/simpletunnel-darwin-amd64"
-      sha256 "19e6a57c086572415ac9dfd3655514c709fd1627f9b16c711ca5397368370bc5"
+      url "https://github.com/simple-tunnel/releases/releases/download/v3.40.2/simpletunnel-darwin-amd64"
+      sha256 "b3452768a7c76008c1af5c54245eac8010b39eaa8d0688c0723e762b34837e23"
     end
   end
 
